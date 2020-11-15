4d6f7e7f7128e7bae788296cdc3aac0a6e9d1d02
smedigo <smedigo@gmail.com>
2020-11-15 14:58:52 -0800

    finishedhmwk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var index = 0;
 var count = 30;
 var interval = setInterval(function () {
     document.getElementById('count').innerHTML = count;
@@ -7,91 +7,124 @@ var interval = setInterval(function () {
         clearInterval(interval);
         document.getElementById('count').innerHTML = 'Done';
         alert("You're out of time!");
+        document.getElementById("container-questions").classList.add("hide");
+        document.getElementById("container-submit").classList.remove("hide");
+    }
+
+    if (index===questionsArray.length) {
+    clearInterval(interval);
+        document.getElementById('count').innerHTML = 'Done';
+        alert("You are done!");
+        document.getElementById("container-questions").classList.add("hide");
+        document.getElementById("container-submit").classList.remove("hide");
     }
 }, 1000);
 
- //var containerquestions="";
-   // document.getElementById("containerquestions").onclick = function() { 
-   // document.getElementById("containerquestions").style.display = "none"; 
-   // document.getElementById("Question").innerHTML="";
- //} 
+var leaderboard =  JSON.parse(localStorage.getItem('storage')) || [];
 
  
-
-
-
-var question1 = {
-    title: "What are Variables in JavaScript?",
-    optA: "A container that stores vaule",
+var questionsArray = [
+     {
+     title: "What are Variables in JavaScript?",
+     optA: "A container that stores vaule",
     optB: "B values",
     optC: "C function",
-    optD: "D array",
-    correctAnswer: "A"
-}
-var question2 = {
-    title: "What are Values in JavaScript?",
+     optD: "D array",
+     correctAnswer: "A container that stores vaule",
+ },
+  {
+     title: "What are Values in JavaScript?",
     optA: "A special characters",
     optB: "B true or false statements",
-    optC: "C documents",
-    optD: "D variables",
-    correctAnswer: "B"
-}
-var question3 = {
-    title: "What is a function in JavaScript?",
-    optA: "A function is a loop",
-    optB: "B function is a type of condition",
-    optC: "C function is small factory within the array that stores a code and it acts when it is called",
-    optD: "D function is prompt",
-    correctAnswer: "C"
-}
+     optC: "C documents",
+     optD: "D variables",
+     correctAnswer: "B true or false statements",
+ },
+ {
+     title: "What is a function in JavaScript?",
+     optA: "A function is a loop",
+     optB: "B function is a type of condition",
+     optC: "C function is small factory within the array that stores a code and it acts when it is called",
+     optD: "D function is prompt",
+     correctAnswer: "C function is small factory within the array that stores a code and it acts when it is called"
+ }
 
-document.querySelector("#Question").innerHTML = question1.title;
-document.querySelector(".btnOption1").innerHTML = question1.optA;
-document.querySelector(".btnOption2").innerHTML = question1.optB;
-document.querySelector(".btnOption3").innerHTML = question1.optC;
-document.querySelector(".btnOption4").innerHTML = question1.optD;
+]
 
-document.querySelector("#Question").innerHTML = question2.title;
-document.querySelector(".btnOption1").innerHTML = question2.optA;
-document.querySelector(".btnOption2").innerHTML = question2.optB;
-document.querySelector(".btnOption3").innerHTML = question2.optC;
-document.querySelector(".btnOption4").innerHTML = question2.optD;
 
-document.querySelector("#Question").innerHTML = question3.title;
-document.querySelector(".btnOption1").innerHTML = question3.optA;
-document.querySelector(".btnOption2").innerHTML = question3.optB;
-document.querySelector(".btnOption3").innerHTML = question3.optC;
-document.querySelector(".btnOption4").innerHTML = question3.optD;
+generateQuestion();
 
 
 
+function generateQuestion () {
 
-document.querySelector(".btnOption1").onclick = function () {
-    //alert("Correct!")
-    if (document.querySelector("#Question").innerHTML == question1.title) {
-        alert("Correct!")
-        document.querySelector("#Question").innerHTML = question2.title
-    }
-    else if (document.querySelector("#Question").innerHTML == question2.title) {
-
-        document.querySelector("#Question").innerHTML = question3.title
-    }
+    document.querySelector(".Question").innerHTML = questionsArray[index].title;
+     document.querySelector(".btnOption1").innerHTML = questionsArray[index].optA;
+     document.querySelector(".btnOption2").innerHTML = questionsArray[index].optB;
+     document.querySelector(".btnOption3").innerHTML = questionsArray[index].optC;
+     document.querySelector(".btnOption4").innerHTML = questionsArray[index].optD;
 
 }
-document.querySelector(".btnOption2").onclick = function () {
-    alert("wrong")
 
-    document.querySelector("#Question").innerHTML = question2.title
-    document.querySelector("#Question").innerHTML = question3.title
 
-    document.querySelector(".btnOption3").onclick = function () {
-        alert("wrong")
-        document.querySelector("#Question").innerHTML = question2.title
-        document.querySelector("#Question").innerHTML = question3.title
+var answerbuttons = document.querySelectorAll(".answerbutton")
+
+for (let i = 0; i < answerbuttons.length ; i++) {
+    const element = answerbuttons[i];
+
+    element.addEventListener("click", function(event){
+
+    var clickvalue = event.target.innerHTML
+
+    if (clickvalue != questionsArray[index].correctAnswer) {
+     
+    count -= 10;
+
     }
-    document.querySelector(".btnOption4").onclick = function () {
-        alert("wrong")
-        document.querySelector("#Question").innerHTML = question2.title
-        document.querySelector("#Question").innerHTML = question3.title
-    }
+
+    index ++;
+
+    generateQuestion()
+
+
+
+       })
+    
 }
+
+document.getElementById("submitbutton").addEventListener("click", function(){
+
+    var userScore = {
+        initals: document.getElementById("initialsId").value,
+        score: count,
+    }
+
+    leaderboard.push(userScore);
+
+    localStorage.setItem("storage", JSON.stringify(leaderboard));
+
+    console.log(localStorage);
+
+    setTimeout(function() {
+
+        document.getElementById("container-submit").classList.add("hide");
+        document.getElementById("container-highScore").classList.remove("hide");
+
+
+    }, 500);
+
+    for (let i = 0; i < leaderboard.length; i++) {
+        const users = leaderboard[i];
+
+        var Li = document.createElement("li");
+
+       Li.innerText=users.initals + " " + users.score;
+       document.getElementById("highScore").appendChild(Li);
+
+        
+    }
+        
+    }
+
+);
+
